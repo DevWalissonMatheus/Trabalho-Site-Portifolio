2d9753390d0ed644ce682b1baa27d4ac2dcd3129
Note-KaTSuo <mwalisom9@gmail.com>
2023-10-22 19:08:27 -0300

    comentando o código e fazendo pequenas mudanças

--- menu-responsivo.js
+++ menu-responsivo.js
@@ -1,44 +1,45 @@
 class MenuResponsivo {
   constructor(respMenu, navBotoes, navLinks) {
-    this.respMenu = document.querySelector(respMenu);
-    this.navBotoes = document.querySelector(navBotoes);
-    this.navLinks = document.querySelectorAll(navLinks);
-    this.activeClass = "active";
+    this.respMenu = document.querySelector(respMenu); // Faz referencia ao seletor menu
+    this.navBotoes = document.querySelector(navBotoes); // Faz referencia aos seletores dos botões
+    this.navLinks = document.querySelectorAll(navLinks); // Faz referencia aos seletores de links
+    this.activeClass = "active"; // Faz referencia a classe active
 
-    this.handleClick = this.handleClick.bind(this);
+    this.handleClick = this.handleClick.bind(this); // Evita erro sobre não encontrar os links
   }
 
-  animateLinks() {
-    this.navLinks.forEach((link, index) => {
-      link.style.animation
-        ? (link.style.animation = "")
+  animacaoLinks() { // Função para ativar a barra lateral
+    this.navLinks.forEach((link, index) => { // Seleciona cada link de forma individual
+      link.style.animation // Verifica se os links possuem alguma animação
+        ? (link.style.animation = "") // Se existir continua normal
         : (link.style.animation = `navLinkFade 0.5s ease forwards ${
-            index / 7 + 0.3
+            index / 7 + 0.3 // Se não existir adiciona uma animação
           }s`);
     });
   }
 
-  handleClick() {
-    this.navBotoes.classList.toggle(this.activeClass);
-    this.respMenu.classList.toggle(this.activeClass);
-    this.animateLinks();
+  handleClick() { // Deixa o botão da barra lateral funcional
+    this.navBotoes.classList.toggle(this.activeClass); // Faz referencia a barra lateral
+    this.respMenu.classList.toggle(this.activeClass); // Faz referencial a animação dos botão
+    this.animacaoLinks(); // Faz referencia a animação dos botões
   }
 
   addClickEvent() {
-    this.respMenu.addEventListener("click", this.handleClick);
+    this.respMenu.addEventListener("click", this.handleClick); // Cria o evento de click no menu
+                                                              // e chama o evento que será ativado
   }
 
-  init() {
+  init() { // Inicia a função
     if (this.respMenu) {
-      this.addClickEvent();
+      this.addClickEvent(); // Se o menu existir
     }
-    return this;
+    return this; // Retorna this
   }
 }
 
-const menuResponsivo = new MenuResponsivo(
+const menuResponsivo = new MenuResponsivo( // Variável que utiliza os seletores
   ".menu-responsivo",
   ".nav-botoes",
   ".nav-botoes li"
 );
-menuResponsivo.init();
+menuResponsivo.init(); // Inicia o menu
--- rolagem-suave.js
+++ rolagem-suave.js
@@ -1,46 +1,49 @@
 const menuBotoes = document.querySelectorAll('body a[href^="#"]');
+// Seleciona todos os links do body que são do tipo ID "#"
+menuBotoes.forEach((item) => { // Verifica se os itens da lista foram clicados
+  item.addEventListener("click", rolagemSuaveID); // Adiciona o evento de click
+});                                               // e chama o evento que será ativado
 
-menuBotoes.forEach((item) => {
-  item.addEventListener("click", rolagemSuaveID);
-});
-
-function getScrollTopByHref(element) {
-  const id = element.getAttribute("href");
-  return document.querySelector(id).offsetTop;
+function getScrollTopByHref(element) { // Função para pegar a posição do scroll
+  const id = element.getAttribute("href"); // Pega a posição do href
+  return document.querySelector(id).offsetTop; // Retorna a posição
 }
 
-function rolagemSuaveID(evento) {
-  evento.preventDefault();
-  const vai = getScrollTopByHref(evento.target) -45;
-  roleParaPosicao(vai);
+function rolagemSuaveID(evento) { // Cria a função de rolagem
+  evento.preventDefault(); // Quando clicar no botão não irá mostrar o nome do id
+  const vai = getScrollTopByHref(evento.target) -45; // Leva para a posição do href com um espaçamento
+  roleParaPosicao(vai); // Chama função para rolar    // de -45 para não ficar atrás da barra de rolagem
 }
 
-function roleParaPosicao(vai) {
+function roleParaPosicao(vai) { // Cria a função para a rolar
   roleScrollAte(0, vai, 1200);
+  // Primeiro parâmetro -> Rolagem horizontal
+  // Segundo parâmetro -> Rolagem vertical
+  // Terceiro parâmetro -> Tempo que leva para rolar em ms
 }
 
-function roleScrollAte(endX, endY, duration) {
-  const startX = window.scrollX || window.pageXOffset;
-  const startY = window.scrollY || window.pageYOffset;
-  const distanceX = endX - startX;
-  const distanceY = endY - startY;
-  const startTime = new Date().getTime();
-
+function roleScrollAte(endX, endY, duration) { // Função levar até a posição
+  const startX = window.scrollX || window.pageXOffset; // Obtém a posição horizontal
+  const startY = window.scrollY || window.pageYOffset; // horizontal e vertical
+  const distanceX = endX - startX; // Calcula a distancia a ser percorrida
+  const distanceY = endY - startY; // horizontalmente e verticalmente
+  const startTime = new Date().getTime(); // Guarda o momento de inicio da animação de rolagem
+  // Define uma duração para a animação, caso não seja especificada a duração padrão será 400 ms
   duration = typeof duration !== "undefined" ? duration : 400;
 
   const easeInOutRolagem = (time, from, distance, duration) => {
-    if ((time /= duration / 2) < 1)
+    if ((time /= duration / 2) < 1) // Cria o efeito suavização com aceleração e desaceleração
       return (distance / 2) * time * time * time * time + from;
     return (-distance / 2) * ((time -= 2) * time * time * time - 2) + from;
   };
 
-  const timer = setInterval(() => {
-    const time = new Date().getTime() - startTime;
-    const newX = easeInOutRolagem(time, startX, distanceX, duration);
-    const newY = easeInOutRolagem(time, startY, distanceY, duration);
+  const timer = setInterval(() => { // Cria um temporizador para atualizar a posição
+    const time = new Date().getTime() - startTime; // Define o temporizador
+    const newX = easeInOutRolagem(time, startX, distanceX, duration); // Calcula as novas posições
+    const newY = easeInOutRolagem(time, startY, distanceY, duration); // usando a função de suavização
     if (time >= duration) {
-      clearInterval(timer);
+      clearInterval(timer); // Quando chega na posição o temporizador será limpo
     }
-    window.scroll(newX, newY);
-  }, 1000 / 60);
+    window.scroll(newX, newY); // Atualiza a posição da parra de rolagem
+  }, 1000 / 60); // Define um tempo padrão para atualizar a posição x e y
 }
